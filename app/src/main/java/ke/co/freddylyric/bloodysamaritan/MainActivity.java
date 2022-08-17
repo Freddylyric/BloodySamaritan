a52878b04d2caca25e63790d901471b0f434f291
Freddy Lyric <freddykk20@gmail.com>
2022-08-17 19:05:51 +0300

    chatbot add commit

--- app/src/main/java/ke/co/freddylyric/bloodysamaritan/MainActivity.java
+++ app/src/main/java/ke/co/freddylyric/bloodysamaritan/MainActivity.java
@@ -1,14 +1,113 @@
 package ke.co.freddylyric.bloodysamaritan;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.google.android.material.navigation.NavigationView;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+import ke.co.freddylyric.bloodysamaritan.models.ChatBotMainActivity;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    private DrawerLayout drawerLayout;
+    private Toolbar toolbar;
+    private NavigationView nav_view;
+
+    private CircleImageView nav_profile_image;
+    private TextView nav_fullname, nav_email, nav_bloodgroup, nav_type;
+
+    private DatabaseReference userRef;
+    
+
+ 
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
+
+
+        TextView backButton = findViewById(R.id.backButton);
+
+
+        toolbar= findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("Bloody Samaritan");
+
+        drawerLayout= findViewById(R.id.drawerLayout);
+        nav_view= findViewById(R.id.nav_view);
+
+
+// open nav bar
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(MainActivity.this, drawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+        //fetch user data from database
+
+        nav_profile_image = nav_view.getHeaderView(0).findViewById(R.id.nav_user_image);
+        nav_fullname = nav_view.getHeaderView(0).findViewById(R.id.nav_user_fullName);
+        nav_email = nav_view.getHeaderView(0).findViewById(R.id.nav_user_email);
+        nav_bloodgroup = nav_view.getHeaderView(0).findViewById(R.id.nav_user_bloodgroup);
+        nav_type = nav_view.getHeaderView(0).findViewById(R.id.nav_user_type);
+
+        userRef = FirebaseDatabase.getInstance().getReference().child("users").child(
+                FirebaseAuth.getInstance().getCurrentUser().getUid()
+        );
+
+
+        userRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if (snapshot.exists()){
+                    String name = snapshot.child("name").getValue().toString();
+                    nav_fullname.setText(name);
+
+                    String email = snapshot.child("email").getValue().toString();
+                    nav_email.setText(email);
+
+                    String bloodGroup = snapshot.child("bloodGroup").getValue().toString();
+                    nav_bloodgroup.setText(bloodGroup);
+
+                    String type = snapshot.child("type").getValue().toString();
+                    nav_type.setText(bloodGroup);
+
+                    String imageUrl = snapshot.child("profilepictureurl").getValue().toString();
+                    Glide.with(getApplicationContext()).load(imageUrl).into(nav_profile_image);
+
+                }
+
+            }
+
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+
+
+        }
+
+
 }
